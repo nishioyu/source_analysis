edc61b1f42330d887e054f02db03ef26cb350417
nishio.math@gmail.com <nishio.math@gmail.com>
2023-11-29 21:19:37 +0900

    hoge

--- crud_extract/extract_crud.py
+++ crud_extract/extract_crud.py
@@ -1,4 +1,6 @@
 import get_sql_operation_type
+import r_extract_crud
+import cud_sql_query
 
 # CRUD抽出処理
 def extract_crud(sql_query):
---
+++ crud_extract/r_extract_crud.py
@@ -0,0 +1,5 @@
+
+
+# SELECTのSQLから最外部に当たるCRUDを抽出する処理
+def r_extract_crud(sql_query):
+    return None
