e036d35d336ebc78ed352e25a8205b2a787ea504
nishio.math@gmail.com <nishio.math@gmail.com>
2023-12-07 21:08:20 +0900

    hoge

--- crud_extract/r_extract_crud.py
+++ crud_extract/r_extract_crud.py
@@ -7,10 +7,14 @@ def r_extract_crud(sql_query):
 
     tables_list = parse.tables
     tables_list_no_ailiases = dict(zip(tables_list,tables_list))
+    # エイリアス付きのテーブルを辞書型で抽出する. ただしkeyが
+    # "JOIN","ON","GRUP BY","LEFT JOIN","INNER JOIN","RIGHT JOIN","FULL OUTER JOIN"
+    # があるものは考えない. サブクエリの中のテーブルも抽出される
     tables_list_ailiases = parse.tables_aliases
     # print(tables_list_no_ailiases)
     # print(tables_list_ailiases)
     tables_dict = {**dict(zip(tables_list,tables_list)), **parse.tables_aliases}
+    print(tables_dict)
 
     columns_list = parse.columns_dict["select"]
     columns_list_no_ailiases = [x for x in columns_list if "." not in x]
@@ -22,9 +26,12 @@ def r_extract_crud(sql_query):
     return_list = []
     if len(tables_list_no_ailiases) == 0:
         return_list.append(("SELECT",tables_list_no_ailiases[0],columns_list_no_ailiases))
+        print("hoge")
 
     for k,v in tables_dict.items():
-        return_list.append(("SELECT",v,[y for y in columns_list_ailiases if k == y.split(".")[0]]))
+        tmp_list = [y for y in columns_list_ailiases if k == y.split(".")[0]]
+        if len(tmp_list) != 0:
+          return_list.append(("SELECT",v,[y for y in columns_list_ailiases if k == y.split(".")[0]]))
 
     return return_list
 
