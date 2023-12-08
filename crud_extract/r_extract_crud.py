a4151f23b5e8a6abad3753c515990873b3422967
nishio.math@gmail.com <nishio.math@gmail.com>
2023-12-09 00:16:44 +0900

    rのcrud抽出処理追加

--- crud_extract/r_extract_crud.py
+++ crud_extract/r_extract_crud.py
@@ -1,6 +1,24 @@
 from sql_metadata import Parser
 import re
 
+def extract_first_select_columns_from_sql(sql_query):
+    # SQL文からSELECTとFROMの間の部分を正規表現で抽出
+    match = re.search(r'SELECT(.*?)FROM', sql_query, re.DOTALL)
+    
+    if match:
+        # 抽出した部分からカラムを取得
+        selected_part = match.group(1).strip()
+        
+        # カンマで分割してリストに格納
+        columns = [column.strip() for column in selected_part.split(',')]
+        return columns
+    else:
+        # SELECTとFROMが見つからない場合はエラー処理などを行うことも考慮できます
+        return None
+
+
+
+
 # SELECTのSQLから最外部に当たるCRUDを抽出する処理
 def r_extract_crud(sql_query):
     parse = Parser(sql_query)
@@ -13,15 +31,23 @@ def r_extract_crud(sql_query):
     tables_list_ailiases = parse.tables_aliases
     tables_dict = {**dict(zip(tables_list,tables_list)), **parse.tables_aliases}
 
-    columns_list = parse.columns_dict["select"]
+    # columns_list = parse.columns_dict["select"]
+    columns_list = extract_first_select_columns_from_sql(sql_query)
+
     columns_list_no_ailiases = [x for x in columns_list if "." not in x]
     columns_list_ailiases = [x for x in columns_list if "." in x]
 
-    return_list = []
-    if len(tables_list_no_ailiases) == 0:
-        return_list.append(("SELECT",tables_list_no_ailiases[0],columns_list_no_ailiases))
-        print("hoge")
+    print(columns_list_no_ailiases)
+    print(columns_list_ailiases)
 
+    return_list = []
+
+    # エイリアスなしカラム用
+    if len(list(tables_list_no_ailiases)) != 0:
+      for x in tables_list_no_ailiases:
+        return_list.append(("SELECT",list(tables_list_no_ailiases)[0],columns_list_no_ailiases))
+
+    # エイリアスありカラム用
     for k,v in tables_dict.items():
         tmp_list = [y for y in columns_list_ailiases if k == y.split(".")[0]]
         if len(tmp_list) != 0:
